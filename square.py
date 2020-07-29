5698aa474343c70bd1c7d6b451c1a80577559db2
Katy Sage <katysagehj@gmail.com>
2020-07-29 14:53:43 -0400

    Completed square exercises

---
+++ square.py
@@ -0,0 +1,7 @@
+
+def square():
+    i=1
+    while i <=5:
+        print("*****")
+        i += 1
+square()
---
+++ square2.py
@@ -0,0 +1,7 @@
+def square2():
+    i=1
+    user_input = int(input("How big is the square? "))
+    while i <= user_input:
+        print("*" * user_input)
+        i += 1
+square2()
